aab07445a213780da4e317ad1f11a0de738ee1ac
TOUGMA07 <tougma07@gmail.com>
2025-10-07 06:10:18 +0000

    Affichage d'une image  à l'écran de l'émulateur

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     Center(
-      child: Text("Bonjour Flutter",
-      textDirection: TextDirection.ltr,),
-    )
+      child: Image.network("https://www.radiologieestrie.ca/wp-content/uploads/2024/12/centre-de-radiologie-de-lestrie-irm-2.jpg"))
   );
 }
