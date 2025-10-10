a61b24e21ee4388e1e2ac287a2ef6e2ffafe96ae
TOUGMA07 <tougma07@gmail.com>
2025-10-10 07:47:55 +0000

    Création de la class widget HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    Center(
-      child: Image.network("https://www.radiologieestrie.ca/wp-content/uploads/2024/12/centre-de-radiologie-de-lestrie-irm-2.jpg"))
-  );
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget{
+const HomePage({super.key});
+
+  @override  
+  Widget build(BuildContext context){
+    return  const Center(
+      child: Text("Home page",
+      textDirection: TextDirection.ltr,)
+    );
+  }
 }
